82f12f658ffc3a423fbc69fdc29c1649a25b4bbf
ClackHack <48455071+ClackHack@users.noreply.github.com>
2021-02-24 19:46:46 -0500

    update exe

--- send.py
+++ send.py
@@ -36,7 +36,7 @@ def transfer(path,base,raw=False):
                 #print(os.path.join(p,ff).replace("\\",'/').replace(base,""))
                 #print("\n")
             paths.extend([os.path.join(p,ff).replace("\\",'/') for ff in f])
-        print(paths)
+        #print(paths)
         conn.sendall(bytes("AMOUNT:"+str(len(paths)),"utf-8"))
         conn.sendall(bytes(SEPERATOR+"FILES:"+SUBSEP.join([ff.replace(base,"").strip("/") for ff in paths]),"utf-8"))
         conn.sendall(bytes(SEPERATOR+"DATA:","utf-8")+bytes(SUBSEP,"utf-8").join([open(i,"rb").read() for i in paths]))
