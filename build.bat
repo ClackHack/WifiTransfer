49748f4b61b3b3eca37332bcf49086e37c1d4d62
ClackHack <48455071+ClackHack@users.noreply.github.com>
2021-02-24 19:19:26 -0500

    new build

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 color 2
-pyinstaller  --onefile --noconsole --icon=biblelogo.ico --log-level INFO gui.py
+pyinstaller  --onefile  --log-level INFO main.py
 RMDIR /S /Q Build
-del gui.spec
-del dist\Bible.exe
-ren dist\gui.exe Bible.exe
+del main.spec
+del dist\wtransfer.exe
+ren dist\main.exe wtransfer
